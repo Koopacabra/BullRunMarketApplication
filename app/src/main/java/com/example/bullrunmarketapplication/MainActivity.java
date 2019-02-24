86a39689ff400eb2d9f8715a4c04b47944661f1f
Koopacabra <criteri4@gmail.com>
2019-02-24 17:21:31 -0500

    //changed app icon to USF Logo; //created beta options button in app bar using a new menu; //adjusted truck selection design; //created button to test navigation from Main to Truck Selection;

--- app/src/main/java/com/example/bullrunmarketapplication/MainActivity.java
+++ app/src/main/java/com/example/bullrunmarketapplication/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.bullrunmarketapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //THIS 'private Button button;' IS FOR TESTING ACTIVITY NAVIGATION; SHOULD BE REMOVED/ALTERED WHEN HEATHER
+    private Button button;
+    //REMOVE/ALTER ABOVE
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //THIS IS FOR TESTING ACTIVITY NAVIGATION; SHOULD BE REMOVED/ALTERED WHEN HEATHER BEGINS DEVELOPMENT
+        button = findViewById(R.id.button_to_truck_selection);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTruckSelection();
+            }
+        });
+        //REMOVE/ALTER ABOVE
     }
+
+    //THIS IS FOR TESTING ACTIVITY NAVIGATION; SHOULD BE REMOVED/ALTERED WHEN HEATHER BEGINS DEVELOPMENT
+    public void openTruckSelection() {
+        Intent intent = new Intent(this, TruckSelection.class);
+        startActivity(intent);
+    }
+    //REMOVE/ALTER ABOVE
 }
--- app/src/main/java/com/example/bullrunmarketapplication/TruckSelection.java
+++ app/src/main/java/com/example/bullrunmarketapplication/TruckSelection.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 
 public class TruckSelection extends AppCompatActivity {
@@ -15,6 +16,13 @@ public class TruckSelection extends AppCompatActivity {
         setContentView(R.layout.activity_truck_selection);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    //function to create the options/overflow menu for the app bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options_button, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
