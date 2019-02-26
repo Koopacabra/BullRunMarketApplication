f14be777210cc96a1876b2b9c5087c3abc514964
hemanthlolla <hemanth.lolla@gmail.com>
2019-02-25 19:39:30 -0500

    PizzaTime, where eating Pizza is always a good time, just added a layout and submenu to the master files.

---
+++ app/src/main/java/com/example/bullrunmarketapplication/pizza_layout.java
@@ -0,0 +1,42 @@
+package com.example.bullrunmarketapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class pizza_layout extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizza_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openpizzatime();
+            }
+        });
+    }
+    public void openpizzatime() {
+        Intent intent = new Intent(this, pizzatime.class);
+        startActivity(intent);
+    }
+
+}
