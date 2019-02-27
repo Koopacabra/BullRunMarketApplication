0d90cd596ace9050b06a7f48df8c6e2f2a652899
Koopacabra <criteri4@gmail.com>
2019-02-27 00:03:51 -0500

    //condensed all layouts to just activities (removed content); //set manifest to ignore backup and app indexing to reduce warnings. //fixed runtime error (additional //) in login XML; //added app_bar layout (not finished); //push is a clean, running install for Hemanth to pull (as requested);

--- app/src/main/java/com/example/bullrunmarketapplication/hyderabadi_layout.java
+++ app/src/main/java/com/example/bullrunmarketapplication/hyderabadi_layout.java
@@ -18,14 +18,6 @@ public class hyderabadi_layout extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
