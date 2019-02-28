803ca426651678f3f27120168969abd95f1aee04
Heather <hislip@mail.usf.edu>
2019-02-28 04:25:01 -0500

    Login and sign up progress

--- app/src/main/java/com/example/bullrunmarketapplication/MainActivity.java
+++ app/src/main/java/com/example/bullrunmarketapplication/MainActivity.java
@@ -1,23 +1,20 @@
 package com.example.bullrunmarketapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnLogin, btnSignup;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.activity_main_screen);
 
         btnLogin = findViewById(R.id.btnLogin);
-        btnSignup = findViewById(R.id.btnSignup);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,12 +23,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
     }
 }
 
