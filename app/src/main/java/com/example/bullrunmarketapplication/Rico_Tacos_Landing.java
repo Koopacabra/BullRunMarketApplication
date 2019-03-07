d1d1f0eef3eeb50dfb7f78d1366fda5a81e8373d
Benjamin Saint Louis <bstlouis95@gmail.com>
2019-03-07 03:58:58 -0500

    Second committ updated java files and xml files to correct naming conventions

---
+++ app/src/main/java/com/example/bullrunmarketapplication/Rico_Tacos_Landing.java
@@ -0,0 +1,78 @@
+package com.example.bullrunmarketapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Rico_Tacos_Landing extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rico_landing);
+
+        //casting toolbar as an actionbar
+        Toolbar toolbar = findViewById(R.id.appBar);
+        setSupportActionBar(toolbar);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openricos();
+            }
+        });
+    }
+    public void openricos() {
+        Intent intent = new Intent(this, Rico_Tacos.class);
+        startActivity(intent);
+    }
+
+    //function to create the options/overflow menu for the app bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options_button, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /*function to determine what happens when an item is selected
+    from the options menu*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //case for Sign Out
+            case R.id.options_signOut:
+                //toast to float Goodbye message on sign out
+                Toast.makeText(getApplicationContext(), "Goodbye!", Toast.LENGTH_SHORT).show();
+                //intent to navigate to Main (sign in activity)
+                Intent signOut = new Intent(this, MainActivity.class);
+                startActivity(signOut);
+                break;
+            //case to go to About
+            case R.id.options_about:
+                //toast to float message going to About activity
+                Toast.makeText(getApplicationContext(), "A little more about us...", Toast.LENGTH_SHORT).show();
+                //intent to navigate to About activity
+                Intent about = new Intent(this, About.class);
+                startActivity(about);
+                break;
+            //case to go to Checkout
+            case R.id.options_cart:
+                //toast to float message going to Cart
+                Toast.makeText(getApplicationContext(), "Does everything look correct?", Toast.LENGTH_SHORT).show();
+                //intent to navigate to Checkout activity
+                Intent checkout = new Intent(this, Checkout.class);
+                startActivity(checkout);
+            default:
+                //unknown error action TBD
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
