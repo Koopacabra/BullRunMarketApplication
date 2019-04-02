827ff2e49b9b5e89ebaebdb47382a9dd743772dd
Benjamin Saint Louis <bstlouis95@gmail.com>
2019-04-02 07:44:07 -0400

    Added add_to_cart to menu buttons added prices to the Strings.xml and updated checkout page

--- app/src/main/java/com/example/bullrunmarketapplication/Italian.java
+++ app/src/main/java/com/example/bullrunmarketapplication/Italian.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.bullrunmarketapplication.repository.CartItemRepository;
@@ -51,48 +52,6 @@ public class Italian extends AppCompatActivity {
         item.setName(name);
         item.setAmount(amount);
         cartItemRepository.saveCartItem(item);
-    }
-
-    //function to create the options/overflow menu for the app bar
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_options_button, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    /*function to determine what happens when an item is selected
-    from the options menu*/
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            //case for Sign Out
-            case R.id.options_signOut:
-                //toast to float Goodbye message on sign out
-                Toast.makeText(getApplicationContext(), "Goodbye!", Toast.LENGTH_SHORT).show();
-                //intent to navigate to Main (sign in activity)
-                Intent signOut = new Intent(this, SelectionScreen.class);
-                startActivity(signOut);
-                break;
-            //case to go to About
-            case R.id.options_about:
-                //toast to float message going to About activity
-                Toast.makeText(getApplicationContext(), "A little more about us...", Toast.LENGTH_SHORT).show();
-                //intent to navigate to About activity
-                Intent about = new Intent(this, About.class);
-                startActivity(about);
-                break;
-            //case to go to Checkout
-            case R.id.options_cart:
-                //toast to float message going to Cart
-                Toast.makeText(getApplicationContext(), "Does everything look correct?", Toast.LENGTH_SHORT).show();
-                //intent to navigate to Checkout activity
-                Intent checkout = new Intent(this, Checkout.class);
-                startActivity(checkout);
-            default:
-                //unknown error action TBD
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     //function to add food items to cart
